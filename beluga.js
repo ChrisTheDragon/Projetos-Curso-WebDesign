44d38351a96640c1a128a898d5bb5c244e52041e
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-02-18 11:24:44 -0300

    DOM

---
+++ beluga.js
@@ -0,0 +1,12 @@
+function calcularMedia() {
+    var total = 0;
+    var qtd = arguments.length;
+    var x = 0;
+
+    while (typeof arguments[x] === 'number') {
+        total += arguments[x];
+        x++;
+    }
+
+    return total / qtd;
+}
