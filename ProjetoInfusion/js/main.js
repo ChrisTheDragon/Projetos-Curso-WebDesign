bc0c885f2ac642a11ac90f13f5a75ae6ca15d68e
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-03-08 12:02:29 -0300

    Js Menu responsivo finalização

--- ProjetoInfusion/js/main.js
+++ ProjetoInfusion/js/main.js
@@ -2,4 +2,10 @@
     var $body = document.querySelector('body');
     $body.classList.remove('no-js');
     $body.classList.add('js');
+
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024,
+    });
 }) ()
