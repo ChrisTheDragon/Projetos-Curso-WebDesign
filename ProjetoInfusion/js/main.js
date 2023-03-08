e69b59ccaba3c04e5d7b31f0a708aac2fcf244bd
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-03-08 19:09:38 -0300

    Js Carrossel inicio

--- ProjetoInfusion/js/main.js
+++ ProjetoInfusion/js/main.js
@@ -8,4 +8,11 @@
         toggleBtn: '.header__btnMenu',
         widthEnabled: 1024,
     });
+
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next',
+    })
 }) ()
